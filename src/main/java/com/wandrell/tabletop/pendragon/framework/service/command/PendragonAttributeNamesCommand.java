baa4112898f474d29fda7732ee1e151b6b35bd06
Bernardo Martínez Garrido <programming@wandrell.com>
2014-07-20 11:25:02 +0200

    Changed the empty writed for the disabled writer.

--- src/main/java/com/wandrell/tabletop/pendragon/framework/service/command/PendragonAttributeNamesCommand.java
+++ src/main/java/com/wandrell/tabletop/pendragon/framework/service/command/PendragonAttributeNamesCommand.java
@@ -6,10 +6,10 @@ import java.util.Collection;
 import com.wandrell.framework.command.ReturnCommand;
 import com.wandrell.tabletop.pendragon.framework.conf.ModelFile;
 import com.wandrell.tabletop.pendragon.framework.model.xml.NamesXMLDocumentReader;
-import com.wandrell.tabletop.pendragon.framework.model.xml.NamesXMLDocumentWriter;
 import com.wandrell.util.PathUtils;
 import com.wandrell.util.file.api.FileHandler;
 import com.wandrell.util.file.impl.xml.DefaultXMLFileHandler;
+import com.wandrell.util.file.impl.xml.DisabledXMLWriter;
 import com.wandrell.util.file.impl.xml.XSDValidator;
 
 public final class PendragonAttributeNamesCommand implements
@@ -24,9 +24,10 @@ public final class PendragonAttributeNamesCommand implements
 	final FileHandler<Collection<String>> file;
 
 	file = new DefaultXMLFileHandler<Collection<String>>(
-		new NamesXMLDocumentWriter(), new NamesXMLDocumentReader(),
-		new XSDValidator(PathUtils.getClassPathResource(Paths
-			.get(ModelFile.VALIDATION_ATTRIBUTE))));
+		new DisabledXMLWriter<Collection<String>>(),
+		new NamesXMLDocumentReader(), new XSDValidator(
+			PathUtils.getClassPathResource(Paths
+				.get(ModelFile.VALIDATION_ATTRIBUTE))));
 
 	return file.read(PathUtils.getClassPathResource(Paths
 		.get(ModelFile.ATTRIBUTE)));
--- src/main/java/com/wandrell/tabletop/pendragon/framework/service/command/PendragonBaseSkillsCommand.java
+++ src/main/java/com/wandrell/tabletop/pendragon/framework/service/command/PendragonBaseSkillsCommand.java
@@ -7,11 +7,11 @@ import com.wandrell.framework.command.ReturnCommand;
 import com.wandrell.tabletop.pendragon.framework.conf.FileToken;
 import com.wandrell.tabletop.pendragon.framework.conf.ModelFile;
 import com.wandrell.tabletop.pendragon.framework.model.xml.SkillsXMLDocumentReader;
-import com.wandrell.tabletop.pendragon.framework.model.xml.SkillsXMLDocumentWriter;
 import com.wandrell.tabletop.pendragon.valuehandler.PendragonSkill;
 import com.wandrell.util.PathUtils;
 import com.wandrell.util.file.api.FileHandler;
 import com.wandrell.util.file.impl.xml.DefaultXMLFileHandler;
+import com.wandrell.util.file.impl.xml.DisabledXMLWriter;
 import com.wandrell.util.file.impl.xml.XSDValidator;
 
 public final class PendragonBaseSkillsCommand implements
@@ -31,9 +31,9 @@ public final class PendragonBaseSkillsCommand implements
 	reader.addRejectedAttribute(FileToken.REPEAT);
 
 	file = new DefaultXMLFileHandler<Collection<PendragonSkill>>(
-		new SkillsXMLDocumentWriter(), reader, new XSDValidator(
-			PathUtils.getClassPathResource(Paths
-				.get(ModelFile.VALIDATION_SKILL))));
+		new DisabledXMLWriter<Collection<PendragonSkill>>(), reader,
+		new XSDValidator(PathUtils.getClassPathResource(Paths
+			.get(ModelFile.VALIDATION_SKILL))));
 
 	return file.read(PathUtils.getClassPathResource(Paths
 		.get(ModelFile.SKILL)));
