7a09a527d0366ba9f99dfaa1661cf0780e34eff5
Bernardo Martínez Garrido <programming@wandrell.com>
2015-02-22 21:09:17 +0100

    Added a FamilyCharacteristicTable outputter and tests for it.

---
+++ src/main/java/com/wandrell/tabletop/pendragon/util/outputter/chargen/FamilyCharacteristicTableYAMLOutputter.java
@@ -0,0 +1,100 @@
+package com.wandrell.tabletop.pendragon.util.outputter.chargen;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.wandrell.tabletop.interval.Interval;
+import com.wandrell.tabletop.pendragon.model.chargen.FamilyCharacteristicTable;
+import com.wandrell.tabletop.pendragon.model.chargen.FamilyCharacteristicTemplate;
+import com.wandrell.tabletop.pendragon.util.outputter.YAMLOutputter;
+import com.wandrell.tabletop.skill.NameAndDescriptor;
+
+public final class FamilyCharacteristicTableYAMLOutputter extends
+        YAMLOutputter<FamilyCharacteristicTable> {
+
+    public FamilyCharacteristicTableYAMLOutputter() {
+        super();
+    }
+
+    private final Map<String, Object> getFamilyCharacteristicMap(
+            final FamilyCharacteristicTemplate characteristic) {
+        final Map<String, Object> data;
+        final Map<String, Object> bonus;
+        final Collection<Map<String, Object>> skills;
+        final Collection<Map<String, Object>> attributes;
+        Map<String, Object> value;
+
+        data = new LinkedHashMap<String, Object>();
+        bonus = new LinkedHashMap<String, Object>();
+        skills = new LinkedList<Map<String, Object>>();
+        attributes = new LinkedList<Map<String, Object>>();
+
+        data.put("name", characteristic.getName());
+
+        for (final Entry<NameAndDescriptor, Integer> entry : characteristic
+                .getSkills().entrySet()) {
+            value = new LinkedHashMap<String, Object>();
+
+            value.put("name", entry.getKey().getName());
+            value.put("descriptor", entry.getKey().getDescriptor());
+            value.put("value", entry.getValue());
+
+            skills.add(value);
+        }
+
+        for (final Entry<String, Integer> entry : characteristic
+                .getAttributes().entrySet()) {
+            value = new LinkedHashMap<String, Object>();
+
+            value.put("name", entry.getKey());
+            value.put("value", entry.getValue());
+
+            attributes.add(value);
+        }
+
+        if (!skills.isEmpty()) {
+            bonus.put("skills", skills);
+        }
+
+        if (!attributes.isEmpty()) {
+            bonus.put("attributes", attributes);
+        }
+
+        if (!bonus.isEmpty()) {
+            data.put("bonus", bonus);
+        }
+
+        return data;
+    }
+
+    @Override
+    protected final Map<String, Object> buildMap(
+            final FamilyCharacteristicTable value) {
+        final Map<String, Object> data;
+        final Collection<Map<String, Object>> intervals;
+        Map<String, Object> interval;
+
+        data = new LinkedHashMap<String, Object>();
+        intervals = new LinkedList<>();
+
+        data.put("name", value.getName());
+        data.put("intervals", intervals);
+
+        for (final Entry<Interval, FamilyCharacteristicTemplate> entry : value
+                .getIntervals().entrySet()) {
+            interval = new LinkedHashMap<String, Object>();
+
+            interval.put("lower_limit", entry.getKey().getLowerLimit());
+            interval.put("family_characteristic",
+                    getFamilyCharacteristicMap(entry.getValue()));
+
+            intervals.add(interval);
+        }
+
+        return data;
+    }
+
+}
---
+++ src/test/java/com/wandrell/tabletop/testing/pendragon/framework/test/integration/outputter/chargen/ITSendFamilyCharacteristicTableYAMLOutputter.java
@@ -0,0 +1,100 @@
+package com.wandrell.tabletop.testing.pendragon.framework.test.integration.outputter.chargen;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Reader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.wandrell.pattern.outputter.Outputter;
+import com.wandrell.pattern.parser.Parser;
+import com.wandrell.tabletop.interval.Interval;
+import com.wandrell.tabletop.pendragon.model.chargen.FamilyCharacteristicTable;
+import com.wandrell.tabletop.pendragon.model.chargen.FamilyCharacteristicTemplate;
+import com.wandrell.tabletop.pendragon.service.ModelService;
+import com.wandrell.tabletop.pendragon.util.outputter.chargen.FamilyCharacteristicTableYAMLOutputter;
+import com.wandrell.tabletop.pendragon.util.parser.chargen.FamilyCharacteristicTableYAMLParser;
+import com.wandrell.tabletop.testing.pendragon.framework.framework.conf.TestModelFileConf;
+import com.wandrell.tabletop.testing.pendragon.framework.framework.conf.factory.TestServiceFactory;
+import com.wandrell.util.ResourceUtils;
+
+public final class ITSendFamilyCharacteristicTableYAMLOutputter {
+
+    private static final Random                        random        = new Random();
+    private static final String                        TEMPLATE_PATH = "target/test_write_FamilyCharacteristicTable_";
+    private final Outputter<FamilyCharacteristicTable> outputter;
+
+    {
+        outputter = new FamilyCharacteristicTableYAMLOutputter();
+    }
+
+    public ITSendFamilyCharacteristicTableYAMLOutputter() {
+        super();
+    }
+
+    @Test
+    public final void testWriteFile() throws Exception {
+        final FamilyCharacteristicTable table;
+        final FamilyCharacteristicTable tableOut;
+        final Parser<Reader, FamilyCharacteristicTable> parser;
+        final ModelService modelService;
+        final Path pathOut;
+
+        modelService = TestServiceFactory.getInstance().getModelService();
+
+        parser = new FamilyCharacteristicTableYAMLParser(modelService);
+
+        table = parser.parse(ResourceUtils
+                .getClassPathReader(TestModelFileConf.FAMILY_CHARACTERISTIC));
+
+        pathOut = Paths.get(TEMPLATE_PATH + getRandomID() + ".yml")
+                .toAbsolutePath();
+
+        outputter.send(table,
+                new BufferedWriter(new FileWriter(pathOut.toFile())));
+
+        tableOut = parser.parse(new BufferedReader(new FileReader(pathOut
+                .toFile())));
+
+        Assert.assertEquals(tableOut.getName(), table.getName());
+
+        assertEquals(tableOut.getIntervals(), table.getIntervals());
+    }
+
+    private final void assertEquals(
+            final Map<Interval, FamilyCharacteristicTemplate> actual,
+            final Map<Interval, FamilyCharacteristicTemplate> expected) {
+        final Iterator<Entry<Interval, FamilyCharacteristicTemplate>> itrActual;
+        final Iterator<Entry<Interval, FamilyCharacteristicTemplate>> itrExpected;
+        Entry<Interval, FamilyCharacteristicTemplate> entryActual;
+        Entry<Interval, FamilyCharacteristicTemplate> entryExpected;
+
+        itrActual = actual.entrySet().iterator();
+        itrExpected = expected.entrySet().iterator();
+
+        while (itrActual.hasNext()) {
+            entryActual = itrActual.next();
+            entryExpected = itrExpected.next();
+
+            Assert.assertEquals(entryActual.getKey(), entryExpected.getKey());
+            Assert.assertEquals(entryActual.getValue().getAttributes(),
+                    entryExpected.getValue().getAttributes());
+            Assert.assertEquals(entryActual.getValue().getSkills(),
+                    entryExpected.getValue().getSkills());
+        }
+    }
+
+    private final Integer getRandomID() {
+        return random.nextInt(Integer.MAX_VALUE);
+    }
+
+}
